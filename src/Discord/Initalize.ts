3645f2b46b663b2312582afc0491c7c30b615b6e
dayrain <blackwaterbread@gmail.com>
2024-03-12 15:26:19 +0900

    refactor: refresh logic

--- src/Discord/Initalize.ts
+++ src/Discord/Initalize.ts
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import { ActivityType, Client, Guild, TextChannel } from "discord.js";
 import { logNormal, logWarning } from "Lib/Log";
-import Config, { saveStorage } from "Config";
+import Config, { getStorage, saveStorage } from "Config";
 import { getNoticeMessage, getRegisterInteractionMessage, getDeleteInteractionMessage } from "./Message";
 import appJson from 'Root/package.json';
 
@@ -15,31 +15,31 @@ export async function initBotPresence(client: Client<true>) {
 
 export async function initRegisterInteractMessages(client: Client<true>) {
     const guilds = client.guilds.cache;
-    const { storage: instances } = Config;
-    if (_.isEmpty(instances)) {
+    const storage = getStorage();
+    if (_.isEmpty(storage)) {
         throw new Error('[App] 등록된 서버가 없습니다.');
     }
     for (const [guildId, guild] of guilds) {
-        const storage = instances.get(guildId);
-        if (storage) {
-            const { channelId, noticeMessageId, registerMessageId, deleteMessageId } = storage.channels.interaction;
+        const server = storage.get(guildId);
+        if (server) {
+            const { channelId, noticeMessageId, registerMessageId, deleteMessageId } = server.channels.interaction;
             const channel = await client.channels.fetch(channelId) as TextChannel;
             if (_.isEmpty(noticeMessageId) || _.isUndefined(noticeMessageId)) {
                 const form = getNoticeMessage();
                 const message = await channel.send(form);
-                storage.channels.interaction.noticeMessageId = message.id;
+                server.channels.interaction.noticeMessageId = message.id;
                 saveStorage();
             }
             if (_.isEmpty(registerMessageId) || _.isUndefined(registerMessageId)) {
                 const form = getRegisterInteractionMessage();
                 const message = await channel.send(form);
-                storage.channels.interaction.registerMessageId = message.id;
+                server.channels.interaction.registerMessageId = message.id;
                 saveStorage();
             }
             if (_.isEmpty(deleteMessageId) || _.isUndefined(deleteMessageId)) {
                 const form = getDeleteInteractionMessage();
                 const message = await channel.send(form);
-                storage.channels.interaction.deleteMessageId = message.id;
+                server.channels.interaction.deleteMessageId = message.id;
                 saveStorage();
             }
         }
@@ -48,7 +48,7 @@ export async function initRegisterInteractMessages(client: Client<true>) {
 }
 
 export function checkUnregisteredServer(guild: Guild) {
-    const { storage } = Config;
+    const storage = getStorage();
     if (!storage.get(guild.id)) {
         logWarning(`[App] ID가 등록되지 않은 서버가 있습니다: [${guild.id}|${guild.name}]`);
     }
