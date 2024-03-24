d8389438bc4f683ea6b1c2e43147392d6cfdb632
dayrain <blackwaterbread@gmail.com>
2024-03-25 02:42:53 +0900

    fixed: reduce server query timeout

--- src/Server/Games/ArmaResistance.ts
+++ src/Server/Games/ArmaResistance.ts
@@ -3,6 +3,7 @@ import { GameDig } from "gamedig";
 import { logError } from "Lib/Log";
 import { ConnectInfo } from "Types";
 import { toEmptySafeObject } from "Lib/Utils";
+import { query } from "Server";
 
 export interface ArmaResistanceServerQueries {
     game: 'armaresistance',
@@ -53,7 +54,7 @@ export interface ArmaResistanceServerQueries {
 export async function queryArmaResistance(connection: ConnectInfo): Promise<ArmaResistanceServerQueries> {
     const { host, port } = connection;
     try {
-        const state: any = await GameDig.query({
+        const state: any = await query({
             // this for 2.01, 1.99 : 'acwa'
             type: 'armaresistance',
             host: host,
--- src/Server/index.ts
+++ src/Server/index.ts
@@ -1,5 +1,14 @@
+import { GameDig, QueryOptions } from "gamedig";
 import { Arma3ServerQueries } from "./Games/Arma3";
 import { ArmaReforgerServerQueries } from "./Games/ArmaReforger";
 import { ArmaResistanceServerQueries } from "./Games/ArmaResistance";
 
 export type ServerQueries = Arma3ServerQueries | ArmaReforgerServerQueries | ArmaResistanceServerQueries;
+
+export function query(options: QueryOptions) {
+    return GameDig.query({
+        ...options,
+        maxRetries: 1,
+        socketTimeout: 3000
+    });
+}
