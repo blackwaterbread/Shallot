4099d5eb4a70ef4c7fb0f14a588869edf45d8762
dayrain <blackwaterbread@gmail.com>
2024-03-31 06:26:06 +0900

    improve: separated config, storage code

--- src/Config.ts
+++ src/Config.ts
@@ -1,10 +1,8 @@
 import _ from 'lodash';
 import fs from 'fs';
 import dotenv from "dotenv";
-import { AvailableGame } from 'Types';
 import { advStringify } from 'Lib/Utils';
 import appJson from 'Root/package.json';
-import { ServerQueries } from 'Server';
 import path from 'path';
 dotenv.config();
 
@@ -19,81 +17,10 @@ export interface AppConfigs {
     serverAutoDeleteCount: number;
 }
 
-export interface DiscordUser {
-    id: string;
-    displayName: string;
-    url: string;
-    avatarUrl: string;
-}
-
-export interface BIConnection {
-    host: string;
-    port: number;
-}
-
-export type BIServerPlayers = Array<{
-    name: string;
-    score?: number;
-    time?: number;
-}>
-
-export interface BIServer {
-    type: AvailableGame;
-    nonce: string;
-    priority: boolean;
-    connect: BIConnection;
-    presetPath: string;
-    discord: {
-        statusEmbedMessageId: string;
-        rconEmbedMessageId: string;
-        owner: DiscordUser;
-    },
-    information: {
-        hostname: string;
-        players: BIServerPlayers;
-        memo: string;
-        addonsHash: string;
-        lastQueries: ServerQueries;
-    },
-    rcon: {
-        port: number;
-        password: string;
-    } | null,
-    connection: {
-        status: boolean;
-        count: number;
-    }
-}
-
-export interface AppStorage {
-    channels: {
-        interaction: {
-            channelId: string;
-            noticeMessageId: string;
-            registerMessageId: string;
-            deleteMessageId: string;
-        },
-        status: {
-            channelId: string;
-        },
-        admin: {
-            channelId: string;
-        }
-    },
-    servers: Map<string, BIServer>; // connectionString, Instance
-}
-
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 const CONFIGS_PATH = path.join(__dirname, '/configs/configs.json');
-const STORAGE_PATH = path.join(__dirname, '/configs/storage.json');
-
 const CONFIGS = JSON.parse(fs.readFileSync(CONFIGS_PATH).toString('utf8')) as AppConfigs;
-const STORAGE = new Map<string, AppStorage>(JSON.parse(fs.readFileSync(STORAGE_PATH).toString('utf8')));
-
-for (const [k, v] of STORAGE) {
-    v.servers = new Map(v.servers);
-}
 
 const { token, appId, staticPath } = CONFIGS;
 
@@ -113,17 +40,7 @@ export function getConfigs() {
     return CONFIGS;
 }
 
-export function getStorage() {
-    return STORAGE;
-}
-
 export function saveConfigs() {
     const p = advStringify(CONFIGS);
     fs.writeFileSync(CONFIGS_PATH, p);
-}
-
-export function saveStorage() {
-    /* it will be problem if server processing many instances */
-    const p = advStringify(Array.from(_.cloneDeep(STORAGE).entries()));
-    fs.writeFileSync(STORAGE_PATH, p);
 }
